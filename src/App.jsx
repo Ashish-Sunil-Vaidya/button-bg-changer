f29ffeb45f6eea8a6196d38f63084b07aa7caaa7
Ashish Vaidya <ashuvaidya2003@gmail.com>
2024-03-12 22:09:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [currentBg, setCurrentBg] = useState(0);
+
+  const bgColors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+    "pink",
+    "white",
+    "black",
+    "gray",
+    "brown",
+    "cyan",
+    "teal",
+    "lime",
+    "lightgreen",
+  ];
+
+  return (
+    <div
+      className="app"
+      style={{
+        backgroundColor: bgColors[currentBg],
+      }}
+    >
+      <h1
+        style={{
+          color:
+            bgColors[currentBg] === "white" || bgColors[currentBg] === "yellow"
+              ? "black"
+              : "white",
+        }}
+      >
+        Background Changer
+      </h1>
+      <div className="buttons-grid">
+        {bgColors.map((color, index) => (
+          <button
+            key={index}
+            style={{
+              backgroundColor: color,
+              color:
+                color === "white" || color === "yellow" ? "black" : "white",
+            }}
+            onClick={() => setCurrentBg(index)}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
